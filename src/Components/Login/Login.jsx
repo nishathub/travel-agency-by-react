b07db9a4ff25ec93f56284711895b9ddeb974527
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2024-03-26 16:51:21 +0600

    loginUser is working

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,15 +2,57 @@ import { Link } from "react-router-dom";
 import Navbar from "../Shared/NavBar/Navbar";
 import facebookLogo from '../../assets/images/icons/fb.png';
 import googleLogo from '../../assets/images/icons/google.png';
+import { useContext, useState } from "react";
+import { TravelContext } from "../../AuthProvider/AuthProvider";
 
 const Login = () => {
 
+    const { loginUser, resetPassword } = useContext(TravelContext);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+    const handleForgotPassword = e => {
+        e.preventDefault();
+        // reset state
+        setErrorMessage('');
+        setSuccessMessage('');
+
+        const loginEmail = document.getElementById('login-email');
+        if (loginEmail.value == '') {
+            return setErrorMessage('enter your email');
+        }
+        resetPassword(loginEmail.value)
+            .then(() => {
+                setSuccessMessage('An email has been sent to your mail inbox')
+            })
+            .catch((error) => {
+                setErrorMessage(error.message)
+            })
+    }
+
     const handleLogin = e => {
-    // get form data
-    const form = new FormData(e.currentTarget);
-    const loginEmail = form.get('email');
-    const loginPassword = form.get('password');
-    
+        e.preventDefault();
+
+        // reset state
+        setErrorMessage('');
+        setSuccessMessage('');
+        // get form data
+        const form = new FormData(e.currentTarget);
+        const loginEmail = form.get('email');
+        const loginPassword = form.get('password');
+
+
+        loginUser(loginEmail, loginPassword)
+            .then((result) => {
+                console.log(result.user);
+                setSuccessMessage('login successfull')
+            })
+            .catch((error) => {
+                console.error(error.message)
+                setErrorMessage(error.message)
+            })
+
+
     }
 
     return (
@@ -22,8 +64,12 @@ const Login = () => {
                 <div className="my-12">
                     <form onSubmit={handleLogin} className="border border-slate-500 rounded-md max-w-lg mx-auto p-8">
                         <h2 className="text-xl font-bold mb-8">Login here</h2>
+                        <div className="my-2">
+                            <p className="text-red-500 text-center">{errorMessage}</p>
+                            <p className="text-green-500 text-center">{successMessage}</p>
+                        </div>
                         <div className="flex flex-col gap-4">
-                            <input className="bg-transparent py-2 border-b outline-none" type="email" name="email" placeholder="Email" />
+                            <input className="bg-transparent py-2 border-b outline-none" type="email" name="email" placeholder="Email" id="login-email" />
                             <input className="bg-transparent py-2 border-b outline-none" type="password" name="password" placeholder="Password" />
                             <div className="flex justify-between">
                                 <div className="flex gap-2 items-center">
@@ -31,7 +77,7 @@ const Login = () => {
                                     <span>Remember Me</span>
                                 </div>
                                 <div>
-                                    <a className="text-pink-500 underline" href="#">Forgot Password</a>
+                                    <a onClick={handleForgotPassword} className="text-pink-500 underline" href="#">Forgot Password</a>
                                 </div>
                             </div>
                             <button className="w-full btn btn-accent" type="submit">Log in</button>
